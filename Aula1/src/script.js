f87fc0700ac8674201a8236255b3c8bc82a77364
jessica santos <jessicassilva608@gmail.com>
2026-02-24 22:14:45 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ Aula1/src/script.js
@@ -0,0 +1,11 @@
+//Exemplos de variável let
+let nome = "Jessica";
+let nome2 = "Davi"
+
+console.log(nome);
+console.log(nome2);
+
+//Exemplos de variável const
+const times = ["Corinthians", "Timão", "Gaviões"];
+
+console.log(times);
