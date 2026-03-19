3d42108e1472594a3a07afe87912af0d10c6bc8f
Jéssica <jessicassilva608@gmail.com>
2026-03-19 19:29:56 -0300

    Aula 16 - Finalizando Alterações em HTML

---
+++ Aula16/src/script.js
@@ -0,0 +1,10 @@
+// Alterando atributos
+const elemento = document.querySelector(".ex1");
+
+elemento.src = "amigo-meme.jpg";
+elemento.width = 400;
+
+// Conteúdo dinâmico (data)
+const elemento2 = document.querySelector("#ex2");
+
+elemento2.innerHTML = "Data atual: " + Date();
