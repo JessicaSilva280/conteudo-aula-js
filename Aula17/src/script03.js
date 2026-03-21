8001a555cdfbf57f2c1d750ca2c4b3c659fd8f38
Jéssica <jessicassilva608@gmail.com>
2026-03-20 22:11:01 -0300

    Aula 17 - Aprendendo Key Events

---
+++ Aula17/src/script03.js
@@ -0,0 +1,21 @@
+// Utilizando o key events
+const conteudo = document.querySelector(".conteudo");
+const campo = document.querySelector("#campo");
+
+campo.addEventListener("keydown", handleTecla);
+
+function handleTecla(evento) {
+  conteudo.innerHTML = "Você digitou: " + evento.key;
+}
+
+// Exemplo de detecção de tecla
+const campo2 = document.querySelector("#campoEnter");
+const conteudo3 = document.querySelector(".conteudo3");
+
+campo2.addEventListener("keydown", handleCondicao);
+
+function handleCondicao(evento) {
+  if (evento.code === "Enter") {
+    conteudo3.innerHTML = "A tecla Enter foi pressionada!";
+  }
+}
